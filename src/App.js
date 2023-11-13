4cf4472ae85e57117b4c30fc2ec8e7b78d7c2c7d
Sophie-Ndobe <ndobes76@gmail.com>
2023-11-13 22:58:30 +0200

    Cleaned-up the project folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <p>This is my React Weather App</p>
+      <p>This is my React Weather App</p>
     </div>
   );
 }
